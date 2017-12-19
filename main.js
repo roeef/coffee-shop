bf84b235a2b801684ceda9c1beb6101b63194c7c
roeef <roeef@mail-wise.com>
2017-12-19 12:38:05 +0200

    INDIVIDUAL EXERCISE

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var coffeeShop = {
     beans: 40,
+    money: 100,
+
 
     drinkRequirements: {
         latte: 10,
@@ -11,15 +13,15 @@ var coffeeShop = {
     makeDrink: function (drinkType) {
 
         // Only allows you make a drink if it's a drink in the drinkRequirements object. Otherwise alert: "Sorry, we don't make DRINKNAME".
-        if (!(drinkType in coffeeShop.drinkRequirements)) {
+        if (!(drinkType in this.drinkRequirements)) {
             alert("Sorry, we don't make " + drinkType);
             return;
         }
 
         //If the drink is part of the drinkRequirements object, reduce the amount of beans by the number of beans required for that drink.
-        var drinkRequirement = coffeeShop.drinkRequirements[drinkType];
-        if (drinkRequirement <= coffeeShop.beans) {
-            coffeeShop.beans -= drinkRequirement;
+        var drinkRequirement = this.drinkRequirements[drinkType];
+        if (drinkRequirement <= this.beans) {
+            this.beans -= drinkRequirement;
         } else {
             // If there are not enough beans to make that drink, alert: "Sorry, we're all out of beans!".
             alert("Sorry, we're all out of beans!");
